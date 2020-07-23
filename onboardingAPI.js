2c88e865d6b526e9a0f3ca9dbd9008327602721f
Ang <angela.li.2019@gmail.com>
2020-07-23 18:40:17 -0400

    get progress

--- onboardingAPI.js
+++ onboardingAPI.js
@@ -2,45 +2,55 @@ import "./airtable.browser.js";
 
 var base = new Airtable({ apiKey: 'ENTER API KEY' }).base('ENTER BASE KEY');
 
-const totalQuestions = 15;
+const questions = ["1a", "1b", "1c", "2a", "2b", "2c", "3a", "3b", "3c", "4a", "4b", "4c", "4d", "5a", "5b"];
+
+//returns whether the input is a valid question
+function validQuestion(question) {
+    return (typeof question === 'string' || question instanceof String) && questions.includes(question);
+}
 
 //given the question, returns the correct answer
-function getCorrectAnswer(question) {
-    if (typeof question === 'string' || question instanceof String) {
-        base('QuestionDB').select({
-            filterByFormula: "{Name}='" + question + "'"
+async function getCorrectAnswer(question) {
+    if (validQuestion(question)) {
+        await base('QuestionDB').select({
+            filterByFormula: "{question}='" + question + "'",
+            maxRecords: 1,
         }).eachPage(function page(records, fetchNextPage) {
             records.forEach(function(record) {
-                return record.get('Correct Answer');
+                console.log(question + ": " + record.get('correct_ans'));
+                return record.get('correct_ans');
             });
         });
     } else {
-        throw "Argument must be of type string";
+        throw "Expected: question";
     }
 }
 
 //given the question and the user answer, return whether user answer was correct
-function checkAnswer(question, answer) {
-    if ((typeof question === 'string' || question instanceof String) && (typeof answer === 'string' || answer instanceof String)) {
+async function checkAnswer(question, answer) {
+    if (validQuestion && (typeof answer === 'string' || answer instanceof String)) {
         return getCorrectAnswer(question).equals(answer);
     } else {
-        throw "Arguments must be of type string";
+        throw "Expected: question, answer";
     }
 }
 
 //gets the progress for the module (or overall, if input == 0)
-function getProgress(userEmail, module) {
+async function getProgress(userEmail, module) {
     if ((typeof userEmail === 'string' || userEmail instanceof String) && (typeof module === 'number' || module instanceof Number)) {
-        var completedQuestions = 0;
-        base('TestUserDB').select({
-            filterByFormula: "{user_email}='" + userEmail + "'"
+        await base('TestUserDB').select({
+            filterByFormula: "{user_email}='" + userEmail + "'",
         }).eachPage(function page(records, fetchNextPage) {
             records.forEach(function(record) {
-                completedQuestions = record.get('Completed Questions');
+                var output;
+                if (module == 0) {
+                    output = record.get('totalProgress');
+                } else {
+                 output = record.get(module + '_progress');
+                }
+                console.log(output);
+                return output;
             });
         });
-        return (completedQuestions/totalQuestions)*100;//return a percentage
-    } else {
-        throw "Arguments must be of type string and of type number";
     }
 }
