3acd29bdeb3aa710733794e8da0de9e6fd239448
Ang <angela.li.2019@gmail.com>
2020-08-05 17:05:24 -0400

    fixedd!

--- onboardingAPI.js
+++ onboardingAPI.js
@@ -1,7 +1,42 @@
-import "./airtable.browser.js";
+var http = require('http');
 
+http.createServer(async function (req, res) {
+  // res.writeHead(200, {'Content-Type': 'text/html'});
+  // res.end('Hello World!');
+
+  console.log("adding new user test 3");
+  await addUser("test 3 email", "test 3 first name", "test 3 last name");
+  console.log("------------------------------------");
+  console.log("updating test 2 email 1c to 1");
+  await updateCompletion("test 2 email", "1c", 1);
+  console.log("------------------------------------");
+  for (var i = 0; i < questions.length; i++) {
+      console.log("RETRIEVING " + questions[i]);
+      await getCorrectAnswer(questions[i]).then(answer => console.log("GOT: " + answer));
+  }
+  console.log("------------------------------------");
+  for (var i = 0; i < 6; i++) {
+     await getProgress('li.10011@buckeyemail.osu.edu', i).then(progress => console.log("GOT: " + progress));
+  }
+  console.log("------------------------------------");
+  await getUserProgress('li.10011@buckeyemail.osu.edu').then(info => console.log("GOT: " + info));
+  console.log("------------------------------------");
+  await checkAnswer("1a", "B").then(ret => console.log("is B correct for 1a? " + ret));
+  console.log("------------------------------------");
+  await checkAnswer("1a", "A").then(ret => console.log("is A correct for 1a? " + ret));
+  console.log("------------------------------------");
+  await getPassword('li.10011@buckeyemail.osu.edu').then(pass => console.log("GOT PASSWORD HASH: " + pass));
+  console.log("------------------------------------");
+  await checkPassword('li.10011@buckeyemail.osu.edu', 'cat').then(ret => console.log("password match? " + ret));
+  console.log("------------------------------------");
+  await updatePassword('test 3 email', 'happy camper');
+}).listen(5000);
+
+
+const bcrypt = require('bcryptjs')
+const saltRounds = 10;
 var Airtable = require('airtable');
-var base = new Airtable({ apiKey: 'ENTER API KEY' }).base('ENTER BASE KEY');
+var base = new Airtable({ apiKey: '' }).base('');
 
 const questions = ["1a", "1b", "1c", "2a", "2b", "2c", "3a", "3b", "3c", "4a", "4b", "4c", "4d", "5a", "5b"];
 
@@ -29,8 +64,9 @@ async function getCorrectAnswer(question) {
                 maxRecords: 1,
             }).firstPage();
             const record = records[0];
+            //console.log(question + ": " + record.get('correct_ans'));
             return record.get('correct_ans');
-        }
+            }
     } catch (err) {
         console.log(err);
     }
@@ -45,7 +81,8 @@ async function getCorrectAnswer(question) {
  */
 async function checkAnswer(question, answer) {
     if (validQuestion && (typeof answer === 'string' || answer instanceof String)) {
-        return getCorrectAnswer(question).equals(answer);
+        const ans = await getCorrectAnswer(question);
+        return ans === answer;
     }
 }
 
@@ -64,8 +101,8 @@ async function getRecordID(userEmail) {
     } catch (err) {
         console.log(err);
     }
-}  
-   
+}
+
 /* *
  *  This function updates whether a user correctly completed a question
  *  @param {String} user email
@@ -74,18 +111,20 @@ async function getRecordID(userEmail) {
  *  @public
  */
 async function updateCompletion(userEmail, question, update) {
-    const id = await getRecordID(userEmail);
-    base('Users').update(id, {
-        [question] : update
-    }, function(err, record) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(record.get(question));
-    });  
-}  
-   
+    await getRecordID(userEmail).then(id => {
+        base('Users').update(id, {
+            [question] : update,
+        }, function(err, record) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            // console.log(record.get(question));
+        })
+    })
+    .catch(":(");
+}
+
 /**
  *  This function returns a user's progress, either overall or for a module.
  *  @param {String} user email
@@ -98,45 +137,44 @@ async function getProgress(userEmail, module) {
         if ((typeof userEmail === 'string' || userEmail instanceof String) && (typeof module === 'number' || module instanceof Number)) {
             const records = await base('Users').select({
                 filterByFormula: "{user_email}='" + userEmail + "'",
+                maxRecords: 1,
             }).firstPage();
             const record = records[0];
-            var output;
-            if (module == 0) {
-                output = record.get('totalProgress');
-            } else {
-             output = record.get(module + '_progress');
-            }
+                    var output;
+                    if (module == 0) {
+                        output = record.get('totalProgress');
+                    } else {
+                        output = record.get(module + '_progress');
+                    }
             return output;
         }
     } catch (err) {
-        console.log(err); 
+        console.log(err);
     }
 }
 
-/**
- * This function returns all progress points of a user
- * @param {String} user email
- * @returns {JSON} a JSON string of a dictionary of the user's progress for each module and in total
- * @public
- */
-async function getUserProgress(userEmail){
+async function getUserProgress(userEmail) {
     try {
         if (typeof userEmail === 'string' || userEmail instanceof String) {
               const records = await base('Users').select({
                   filterByFormula: "{user_email}='" + userEmail + "'",
+                  maxRecords: 1,
               }).firstPage();
-              const record = records[0];
-              var outputs = {};
-              for (var i = 0; i < 6; i++) {
-                  var output;
-                  if (i == 0){
-                      output = record.get('totalProgress');
-                  } else {
-                      output = record.get(i + '_progress');
-                  }
-                  outputs[i] = output
-              }
-              return JSON.stringify(outputs);
+                const record = records[0];
+                  //console.log(record);
+                      var outputs = {};
+                      for (var i = 0; i < 6; i++) {
+                          var output;
+                          if (i == 0) {
+                              output = record.get('totalProgress');
+                          } else {
+                              output = record.get(i + '_progress');
+                          }
+                          outputs[i] = output
+                      }
+                      //console.log(outputs);
+                      //console.log(record.get('totalProgress'));
+                      return JSON.stringify(outputs);
           }
     } catch (err) {
         console.log(err);
@@ -147,13 +185,27 @@ async function addUser(userEmail, firstName, lastName) {
     base('Users').create({
         "user_email": userEmail,
         "user_first_name": firstName,
-        "user_last_name": lastName
-       }, function(err, record) {
+        "user_last_name": lastName,
+        "1a":0,
+        "1b":0,
+        "1c":0,
+        "2a":0,
+        "2b":0,
+        "2c":0,
+        "3a":0,
+        "3b":0,
+        "3c":0,
+        "4a":0,
+        "4b":0,
+        "4c":0,
+        "5a":0,
+        "5b":0,
+        "5c":0,
+    }, function(err, record) {
         if (err) {
             console.error(err);
             return;
         }
-        //console.log(record.getId());
     });
 }
 
@@ -161,4 +213,60 @@ async function addUsers(userEmails, firstNames, lastNames) {
     for (var i = 0; i < userEmails.length; i++) {
         addUser(userEmails[i], firstNames[i], lastNames[i]);
     }
+}
+
+async function getPassword(userEmail) {
+    try {
+        if (typeof userEmail === 'string' || userEmail instanceof String) {
+            const records = await base('Users').select({
+                filterByFormula: "{user_email}='" + userEmail + "'",
+                maxRecords: 1,
+            }).firstPage();
+            const record = records[0];
+            return record.get('Hashword');
+            }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function checkPassword(userEmail, userInput) {
+    if ((typeof userEmail === 'string' || userEmail instanceof String) && (typeof userInput === 'string' || userInput instanceof String)) {
+        const hash = await getPassword(userEmail);
+        console.log("checking " + hash);
+        const resp = await bcrypt.compare(userInput, hash);
+        // console.log(resp);
+        return resp;
+    }
+}
+
+async function updatePassword(userEmail, newPass) {
+  var hashword;
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+        if (err) {
+            throw err
+        } else {
+            bcrypt.hash(newPass, salt, function (err, hash) {
+                if (err) {
+                    throw err
+                } else {
+                    console.log("hash successful");
+                    console.log(newPass + " hashed to " + hash);
+                    hashword = hash;
+                }
+            });
+        }
+    });
+    getRecordID(userEmail).then(id => {
+        base('Users').update(id, {
+            Hashword : hashword,
+        }, function(err, record) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            // console.log(record.get('Hashword'));
+        })
+    });
+
 }
